a00f4a924c8a8e18674f013ed612c0cfd47e25e2
Yuri Nogueira <personal@yurinogueira.dev.br>
2023-11-30 22:01:59 -0300

    chore: add deploy-to-gh.js

---
+++ scripts/deploy-to-gh.js
@@ -0,0 +1,23 @@
+import { execa } from "execa";
+import * as fs from "fs";
+
+(async () => {
+    try {
+        await execa("git", ["checkout", "--orphan", "gh-pages"]);
+        console.log("Building started...");
+        await execa("npm", ["run", "build"]);
+        const folderName = fs.existsSync("dist/frontend") ? "dist" : "build";
+        await execa('cp', ['./CNAME', './dist/frontend/CNAME']);
+        await execa("git", ["--work-tree", folderName, "add", "--all"]);
+        await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+        console.log("Pushing to gh-pages...");
+        await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+        await execa("rm", ["-r", folderName]);
+        await execa("git", ["checkout", "-f", "main"]);
+        await execa("git", ["branch", "-D", "gh-pages"]);
+        console.log("Successfully deployed, check your settings");
+    } catch (e) {
+        console.log(e.message);
+        process.exit(1);
+    }
+})();
